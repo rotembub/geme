d5e623ef2a1427490e99c6793d1c648fef496ccb
rotem <rotembub@gmail.com>
2021-10-27 10:30:53 +0300

    basics

--- js/main.js
+++ js/main.js
@@ -23,17 +23,17 @@ function init() {
 
 }
 
-var gKeywords = { 'happy': 12, 'funny puk': 1 }
-var gImgs = [{ id: 1, url: 'img/popo.jpg', keywords: ['happy'] }];
-var gMeme = {
-    selectedImgId: 5,
-    selectedLineIdx: 0,
-    lines: [
-        {
-            txt: 'I never eat Falafel',
-            size: 20,
-            align: 'left',
-            color: 'red'
-        }
-    ]
+
+function loadImages() {
+    var elGallery = document.querySelector('.img-gallery');
+    var strHtml = '';
+
 }
+
+function openEditor() {
+    
+}
+
+
+
+
---
+++ js/services/meme.services.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var gKeywords = { 'happy': 12, 'funny puk': 1 }
+
+
+// var gImgs = [{ id: 1, url: 'img/popo.jpg', keywords: ['happy'] }];
+var gImgs = [
+    {
+        id: 1,
+        url: '...meme-imgs/1.jpg',
+        keywords: ['happy', 'funny'],
+    },
+];
+
+var gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I never eat Falafel',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ]
+}
